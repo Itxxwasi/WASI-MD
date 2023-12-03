6645caa71e7e90b13a889c097303384c87fbec4d
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-03 20:12:24 +0500

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✅ Se desactivo el Bot en este grupo')
+    m.reply('✅ The bot has been deactivated for this chat')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
