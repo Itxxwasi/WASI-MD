120dbbfe942bbb3dd406fca219051e2c485e2679
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-03 21:57:41 +0500

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅ Bot activo  en este grupo')   
+    m.reply('✅ The Bot is now active for this chat')   
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
