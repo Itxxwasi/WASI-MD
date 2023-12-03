26762907823249af42ec7bdb6c9684d7d1397fda
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-03 20:14:39 +0500

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -1,16 +1,15 @@
-//import db from '../lib/database.js'
 
 let handler = async (m, { conn, usedPrefix }) => {
     let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
     let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     
     m.reply(`
-≡ *USUARIOS BANEADOS*
+≡ *USERS BANNED*
 
 ▢ Total : *${users.length}* 
 
 ${users ? '\n' + users.map(([jid], i) => `
-${i + 1}. ${conn.getName(jid) == undefined ? 'Desconocido' : conn.getName(jid)}
+${i + 1}. ${conn.getName(jid) == undefined ? 'UNKNOWN' : conn.getName(jid)}
 ▢ ${jid}
 `.trim()).join('\n') : ''}
 `.trim())
