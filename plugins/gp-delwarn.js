ef9a0ce6c559eac5f67719125cd9c05a6cec257f
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-03 22:24:43 +0500

    Update gp-delwarn.js
    φའįՌ↻Ɛ-βOT-♏D

--- plugins/gp-delwarn.js
+++ plugins/gp-delwarn.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `✳️ Etiqueta o menciona a alguien`
-        if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+        if (!who) throw `✳️ Tag or mention someone`
+        if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
          global.db.data.users[who].warn -= 1
@@ -12,9 +12,9 @@ let handler = async (m, { conn, args, groupMetadata}) => {
          
 ▢ Warns: *-1*
 ▢ Warns total: *${warn - 1}*`)
-         m.reply(`✳️ Un admin redujo su advertencia, ahora tienes *${warn - 1}*`, who)
+         m.reply(`✳️ An admin lowered their warning, now you have *${warn - 1}*`, who)
          } else if (warn == 0) {
-            m.reply('✳️ El usuario no tiene ninguna advertencia')
+            m.reply('✳️ The user has no warning')
         }
 
 }
