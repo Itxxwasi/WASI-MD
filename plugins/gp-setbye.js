602ce2b066961334b17a2bac3802a6dbc58ad0bf
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-03 22:22:58 +0500

    Update gp-setbye.js
    PЯIПᄃΣ-BӨƬ-MD

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -3,8 +3,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sBye = text
-    m.reply('✅ Se estableció el mensaje de despedida')
-  } else throw `✳️ Ingrese el mensaje\n@user (mención)`
+    m.reply('✅ The farewell message was established')
+  } else throw `✳️ enter the message\n@user (mención)`
 }
 handler.help = ['setbye <text>']
 handler.tags = ['group']
