4e15f1401cb01d747e21c021760103929e7a899f
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-04 06:59:44 +0500

    Update fun-tomp3.js
    φའįՌ↻Ɛ-βOT-♏D

--- plugins/fun-tomp3.js
+++ plugins/fun-tomp3.js
@@ -1,21 +1,17 @@
-
 import { toAudio } from '../lib/converter.js'
+
 let handler = async (m, { conn, usedPrefix, command }) => {
-    try {
     let q = m.quoted ? m.quoted : m
-   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-   // if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`
+   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+    if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
     let media = await q.download?.()
-    if (!media) throw '❎ Error al descargar medios'
+    if (!media) throw '❎ Failed to download media'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error al convertir'
+    if (!audio.data) throw '❎ Error converting'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
-    } catch (e) {
-        m.reply(`✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`)
-   }
 }
 handler.help = ['tomp3']
 handler.tags = ['fun']
-handler.command = ['tomp3', 'mp3', 'toudio'] 
+handler.command = /^to(mp3|a(udio)?)$/i
 
 export default handler
