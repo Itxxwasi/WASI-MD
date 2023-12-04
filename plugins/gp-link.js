ed42fe5fa65aab5d4aebff7b76cd3b938c98a0a9
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-04 07:01:19 +0500

    Update gp-link.js
    φའįՌ↻Ɛ-βOT-♏D

--- plugins/gp-link.js
+++ plugins/gp-link.js
@@ -2,13 +2,13 @@ import { areJidsSameUser } from '@whiskeysockets/baileys'
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Sólo se puede usar en grupos'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Can only be used in groups'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants no está definido :('
+    if (!('participants' in groupMetadata)) throw 'participants is not defined :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw '✳️ No estoy en ese grupo :('
-    if (!me.admin) throw '✳️ No soy administrador'
+    if (!me) throw '✳️ I am not in that group :('
+    if (!me.admin) throw '✳️ I am not an administrator'
     m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
 }
 handler.help = ['link']
