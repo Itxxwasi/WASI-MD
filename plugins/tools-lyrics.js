9a1b2cecb3ada2a2bec51b4fbb70f32bcf38846b
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-04 07:07:24 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️ tools-lyrics.js

--- plugins/tools-lyrics.js
+++ plugins/tools-lyrics.js
@@ -1,19 +1,21 @@
+import fetch from 'node-fetch'
 
-import fg from 'api-dylux'
 let handler = async (m, {conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-   if (!teks) throw `✳️ Ingresa el título de una canción`
+   if (!teks) throw `✳️ Enter the name of the song`
    try {
- let res = await fg.lyrics(text);
- let mes = `▢ *${res.title}*
-▢ *${res.artist}*
-
-▢ ${res.lyrics}`;
-    conn.sendFile(m.chat, res.thumb, 'img.png', mes, m);
+  let res = await fetch(global.API('https://some-random-api.com', '/lyrics', { title: teks }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.thumbnail.genius) throw json
+  conn.sendFile(m.chat, json.thumbnail.genius, null, `
+▢ *${json.title}*
+*${json.author}*\n
+${json.lyrics}`, m)
+m.react(done)
 } catch (e) {
 	m.react(error)
-} 
-
+	} 
 }
 handler.help = ['lyrics']
 handler.tags = ['tools']
