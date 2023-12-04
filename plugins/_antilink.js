465ab64324a2dcfd3ce3ee23003fbb154b649d34
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-04 07:29:16 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -17,7 +17,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
         await conn.reply(m.chat, `*≡ 🛡️Link Detected❎*
             You have violated the group rule
 We do not allow links from other groups 
-bye bye *@${m.sender.split('@')[0]}*  you will be kicked out of the group ${isBotAdmin ? '' : '\n\nIM not an admin so I canT expel you :"v'}`, null, { mentions: [m.sender] } )
+bye bye 👋🏻 *@${m.sender.split('@')[0]}*  you will be kicked out of the group ${isBotAdmin ? '' : '\n\nIM not an admin so I canT expel you :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
