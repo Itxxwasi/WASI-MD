f5cf0e0c562752b6ae89b3f19cac9564324599af
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-04 08:35:30 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/mention.js
+++ plugins/mention.js
@@ -3,7 +3,7 @@ handler.all = async function (m, conn) {
     var vn = "https://github.com/PRINCE-GDS/THE-PRINCE-BOTraw/main/Assets/mp3/Audio5.mp3"
     let url = "https://chat.whatsapp.com/GWJkAJSgbv27sGOMLAzMDS"
     let murl = "https://instagram.com/prince_gds?igshid=OGQ5ZDc2ODk2ZA=="
-    let hash = global.botname
+    let hash = global.princebot
     let img = "https://i.imgur.com/8ltcrED.jpeg"
     let num = "923042205427"
 
