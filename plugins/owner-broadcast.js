ebc89d4b5ee6586a93099a3c96e103441e09a515
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-04 06:55:58 +0500

    Update owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `✅ Transmision realizada *Total:* ${chats.length} chats`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `*TRANSMISIÓN ┃ STAFF*\n_____________________\n ${teks} ` ), true).catch(_ => _)
-  m.reply('✅ Se transmitió a todos los chats :)')
+  conn.reply(m.chat, `✅ BROADCAST done *Total:* ${chats.length} chats`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `*BROADCAST ┃ OWNER*\n_____________________\n ${teks} ` ), true).catch(_ => _)
+  m.reply('✅ Broadcast to all chats :)')
 }
 handler.help = ['tx']
 handler.tags = ['owner']
