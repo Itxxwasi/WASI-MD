0ece6fda24819bbc38ddb5aa7fdc15b1183a4de7
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-10 12:53:05 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,26 +1,26 @@
 
 import { addExif } from '../lib/sticker.js'
 let handler = async (m, { conn, text, args }) => {
-  if (!m.quoted) throw 'Responde a un sticker'
+  if (!m.quoted) throw 'respond to a sticker'
   let stiker = false
        let stick = args.join(" ").split("|");
        let f = stick[0] !== "" ? stick[0] : packname;
        let g = typeof stick[1] !== "undefined" ? stick[1] : author;
   try {
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw 'Responde a un sticker'
+    if (!/webp/.test(mime)) throw 'respond to a sticker'
     let img = await m.quoted.download()
-    if (!img) throw 'Responde a un sticker!'
+    if (!img) throw 'Responde to sticker!'
     stiker = await addExif(img, f, g)
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, null, rpl)
-     else throw 'La conversión falló'
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, null)
+     else throw 'conversion failed'
   }
 }
-handler.help = ['take <nombre>|<autor>']
+handler.help = ['take <name>|<author>']
 handler.tags = ['sticker']
 handler.command = ['take', 'wm'] 
 
