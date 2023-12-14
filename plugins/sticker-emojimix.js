dd6bf32726313fc311a5c3e5f1f572e2b8f54d3f
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-14 08:30:26 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -3,8 +3,8 @@ import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
-if (!args[0]) throw `📌 Ejemplo : ${usedPrefix + command} 😎+🤑`
-if (!text.includes('+')) throw  `✳️ Separe el emoji con un *+* \n\n📌 Ejemplo : \n*${usedPrefix + command}* 😎+🤑`
+if (!args[0]) throw `🎴 Example: ${usedPrefix + command} 😎+🥰`
+if (!text.includes('+')) throw  `✳️ Separate the emoji with a *+* sign\n\n📌 Example: \n*${usedPrefix + command}* 😎+🥰`
 let [emoji, emoji2] = text.split`+`
 let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`) ).json()  
 for (let res of anu.results) {
@@ -14,7 +14,7 @@ conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
 
 handler.help = ['emojimix <emoji+emoji>']
 handler.tags = ['sticker']
-handler.command = ['emojimix'] 
-handler.diamond = true
+handler.command = ['emojimix', 'emix'] 
+handler.diamond = false
 
 export default handler
