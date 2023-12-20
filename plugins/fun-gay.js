0fd8517d737f4ba6e5fe3e5eca892b366e865ccf
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2023-12-20 11:15:57 +0500

    🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -1,9 +1,10 @@
 let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let name = conn.getName(who)
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png'), 
-  }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nQuién quiere violar a este gay? `, m)
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/gay', {
+    avatar: pp, 
+  }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nWho wants to rape this gay☠️? `, m)
 }
 
 handler.help = ['gay @user']
