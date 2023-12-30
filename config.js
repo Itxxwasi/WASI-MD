8d2bfb6ca3d8c220472f438e1f05267b408b4847
Itxxwasi <142110420+Itxxwasi@users.noreply.github.com>
2023-12-30 10:33:43 +0500

    ༒︎𝙒𝘼𝙎𝙄-𝙈𝘿-𝘽𝙊𝙏༒︎

---
+++ config.js
@@ -0,0 +1,68 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+//Owner Numbers 
+global.owner = [
+  ['923135673658', 'wasi', false],
+  [''], 
+  [''],
+]
+
+//global.pairingNumber = "" //put your bot number here
+global.mods = ['923305673658'] 
+global.prems = ['923305673658', '923305673658', '923305673658']
+global.allowed = ['923305673658']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz', 
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api.fgmods.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zenzapis.xyz': '675e34de8a', 
+  'https://api.fgmods.xyz': 'dEBWvxCY'
+}
+
+// Sticker WM
+global.botname = '𝗪𝗔𝗦𝗜-𝗠𝗗'
+global.princebot = '🛡️𝗪𝗔𝗦𝗜 𝗧𝗘𝗖𝗛🛡️'
+global.packname = '𝗪𝗔𝗦𝗜♥️' 
+global.author = '𝗧𝗘𝗖𝗛♥️' 
+global.princeig = 'https://www.instagram.com' 
+global.princegp = 'https://chat.whatsapp.com/IhDUWhpX6OWI5q0cMh7tEj'
+global.menuvid = ''
+global.Princesc = '' 
+global.princeyt = 'https://youtube.com/@wasitech1'
+global.Princelog = 'https://i.imgur.com/ujxeU8g.jpeg'
+global.thumb = fs.readFileSync('./Assets/wasi.png')
+
+global.wait = '*♻️ _𝙶𝙴𝚃𝚃𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝙵𝙸𝙻𝙴 𝚆𝙰𝙸𝚃..._*\n*▰▰▰▱▱▱▱▱*'
+global.imgs = '*🖼️ _𝙶𝙴𝚃𝚃𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 ɪᴍᴀɢᴇs 𝚆𝙰𝙸𝚃..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '♻️'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.xmoji = '🌀' 
+
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
