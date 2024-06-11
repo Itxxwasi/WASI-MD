d93e125767555ef617716910be4a3de1964ade19
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2024-06-11 12:56:11 +0500

    Add files via upload

---
+++ plugins/img-asupan.js
@@ -0,0 +1,16 @@
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+	
+    let img = await conn.getFile(global.API('fgmods', '/api/img/asupan-la', { }, 'apikey'))
+    let asupan = img.data
+    conn.sendFile(m.chat, asupan, 'vid.mp4', `✅ Resultado`, m)
+    m.react(dmoji)
+    
+}
+handler.help = ['tvid']
+handler.tags = ['img']
+handler.command = ['asupan', 'tvid', 'videos', 'vid', 'video']
+handler.premium = false
+handler.diamond = true
+
+export default handler
