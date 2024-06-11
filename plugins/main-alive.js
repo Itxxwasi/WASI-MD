2a9c501de7bb5123f3c504731187713e6b7156cc
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2024-06-11 12:03:16 +0500

    Wasi md

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,3 +1,51 @@
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+
+    // Sound
+    let name = m.pushName || conn.getName(m.sender)
+    var vn = "https://github.com/Itxxwasi/WASI-MD/raw/main/Assets/mp3/Audio5.mp3"
+    let url = "https://github.com/Itxxwasi/WASI-MD"
+    let murl = "https://youtube.com/@wasitech1"
+    let img = "https://i.imgur.com/LVcnnmt.png"
+    let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+    let doc = {
+        audio: {
+          url: vn
+        },
+        mimetype: 'audio/mp4',
+        ptt: true,
+        waveform:  [100, 0, 100, 0, 100, 0, 100],
+        fileName: "Guru",
+    
+        contextInfo: {
+          mentionedJid: [m.sender],
+          externalAdReply: {
+          title: "THE-PRINCE-BOT IS ALIVE",
+          body: "PRINCE BOT",
+          thumbnailUrl: img,
+          sourceUrl: 'https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP',
+          mediaType: 1,
+          renderLargerThumbnail: true
+          }}
+      };
+    
+      await conn.sendMessage(m.chat, doc, { quoted: con });
+    
+    }
+    
+    handler.help = ['alive']
+    handler.tags = ['main']
+    handler.command = /^(alive)$/i 
+
+    export default handler;
+
+
+
+
+
+
+
+/** 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
     // Sound
@@ -37,3 +85,4 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     handler.command = /^(alive)$/i 
 
     export default handler;
+**/
